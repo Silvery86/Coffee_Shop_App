9125b7cdf81eb3d63cf6408f9df9afa8f5db2e25
giang8692@gmail.com <giang8692@gmail.com>
2024-03-05 05:30:03 +0700

    upload via internet

---
+++ src/screens/FavortitesScreen.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FavortitesScreen = () => {
+  return (
+    <View>
+      <Text>FavortitesScreen</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({})
+
+export default FavortitesScreen
